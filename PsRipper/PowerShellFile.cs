7b76499e86371e04cb2bd7d13a264fb3c7246291
Matt McNeil <hmqcnoesy@gmail.com>
2015-12-06 06:24:53 -0700

    changed VLC in powershell script to use Handbrake instead to automatically crop borders in wmv files.

--- PsRipper/PowerShellFile.cs
+++ PsRipper/PowerShellFile.cs
@@ -15,8 +15,8 @@ namespace PsRipper
                     $outputFileName = [System.IO.Path]::GetFileNameWithoutExtension($inputFile.FullName) + "".mp4"";
                     $outputFileName = [System.IO.Path]::Combine($inputFile.DirectoryName, $outputFileName);
      
-                    $processName = ""c:\program files (x86)\VideoLAN\VLC\vlc.exe""
-                    $processArgs = ""-I dummy -vvv `""$($inputFile.FullName)`"" --sout=#transcode{{vcodec=`""h264`"",vb=`""0`"",scale=`""0`"",acodec=`""mp3`"",ab=`""40`"",channels=`""1`"",samplerate=`""44100`""}}:standard{{access=`""file`"",mux=`""mp4`"",dst=`""$outputFileName`""}} vlc://quit""
+                    $processName = ""c:\program files (x86)\Handbrake\HandbrakeCLI.exe""
+                    $processArgs = ""-i `""$($inputFile.FullName)`"" -o `""$outputFileName`"" --ab 64""
                     $outputFileName
                     start-process $processName $processArgs -WindowStyle Hidden -wait
                 }}", saveLocation);
