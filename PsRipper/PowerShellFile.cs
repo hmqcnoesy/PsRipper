a0050fde8521860fab21fcc8d138de7ba13d42c5
Matt McNeil <hmqcnoesy@gmail.com>
2015-12-07 13:02:05 -0700

    changed default quality setting in Handbrake to 30, which allows for highly compressed .mp4 files but still suitable for screencasts.

--- PsRipper/PowerShellFile.cs
+++ PsRipper/PowerShellFile.cs
@@ -9,14 +9,22 @@ namespace PsRipper
         internal static void AddConversionScript(string saveLocation)
         {
             var script = string.Format(@"
+                if (Test-Path ""c:\program files\handbrake\HandbrakeCLI.exe"") {{
+                    $env:PATH += ""c:\program files\handbrake""
+                }} elseif (Test-Path ""c:\program files (x86)\handbrake\HandbrakeCLI.exe"") {{
+                    $env:PATH += ""c:\program files (x86)\handbrake""
+                }} else {{
+                    Write-Host ""Couldn't find Handbrake.  Exiting.""
+                }}
+
                 cd ""{0}""
                 foreach ($inputFile in get-childitem -recurse -Filter *.wmv)
                 {{ 
                     $outputFileName = [System.IO.Path]::GetFileNameWithoutExtension($inputFile.FullName) + "".mp4"";
                     $outputFileName = [System.IO.Path]::Combine($inputFile.DirectoryName, $outputFileName);
      
-                    $processName = ""c:\program files (x86)\Handbrake\HandbrakeCLI.exe""
-                    $processArgs = ""-i `""$($inputFile.FullName)`"" -o `""$outputFileName`"" --ab 64 -e x264""
+                    $processName = ""HandbrakeCLI.exe""
+                    $processArgs = ""-i `""$($inputFile.FullName)`"" -o `""$outputFileName`"" --ab 64 -e x264 -q 30""
                     $outputFileName
                     start-process $processName $processArgs -WindowStyle Hidden -wait
                 }}", saveLocation);
